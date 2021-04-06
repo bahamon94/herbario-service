760fe1b28f7d1ad9cf95c18a2bcd34a88135c701
Brayan Bahamon <desarrollador.junior3@interedes.com.co>
2021-04-06 16:23:49 -0500

    port server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1333),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4101f51c6fab85a4b996a17dc5e645d9'),
