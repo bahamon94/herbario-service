84d5af276b5b0a134d13964725948f6de7ea5277
Brayan Bahamon <desarrollador.junior3@interedes.com.co>
2021-05-18 19:46:42 -0500

    cargue masivo de informacion

--- api/registro-especimen/controllers/registro-especimen.js
+++ api/registro-especimen/controllers/registro-especimen.js
@@ -1,8 +1,25 @@
 'use strict';
 
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-
-module.exports = {};
+module.exports = {
+    async uploadFile(ctx) {
+        
+            let data;
+            try {
+                let fileTransformedToArray = ctx.request.body //preparing empty array for storing entities.
+                fileTransformedToArray.forEach( async entity=> {
+                    console.log('REGISTRANDO->',entity);
+                   await strapi.services['registro-especimen'].create(entity);
+                });
+                return true
+        
+            } catch (error) {
+                return false
+            }
+            
+        },
+};
